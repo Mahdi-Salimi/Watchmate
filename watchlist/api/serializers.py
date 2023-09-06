129cb45db8a8a40a4f0fa70ab632801542e9f6a6
Mahdi-Salimi <mahdisalimi.dev@gmail.com>
2023-09-06 10:04:23 +0330

    Model Serializer
    using model serializer instead of serializer
    and adding custom serializer fields

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,22 +1,18 @@
 from rest_framework import serializers
 from watchlist.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
     
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    len_name= serializers.SerializerMethodField()
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        
+    def get_len_name(self, object):
+        length = len(object.name)
+        return length
+        
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('name and description should not be the same')
@@ -27,4 +23,33 @@ class MovieSerializer(serializers.Serializer):
             raise serializers.ValidationError('name must be at least 2 characters')
         else :
             return value
+        
+    
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+    
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+    
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('name and description should not be the same')
+#         return data
+    
+#     def validate_name(self, value):
+#         if len(value) < 2 :
+#             raise serializers.ValidationError('name must be at least 2 characters')
+#         else :
+#             return value
         
